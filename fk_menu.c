aca6777031ec8936408220d0f036e22e0cf20c4d
kane159 <kane12369@gmail.com>
2023-08-13 01:04:57 +0800

    chinese add menu fix

--- fk_menu.c
+++ fk_menu.c
@@ -64,9 +64,9 @@
 
 #define MENU_FONT_NAME_TITLE        "/usr/games/layouts/Bigcup/unifont-15.0.01.ttf"
 #define MENU_FONT_SIZE_TITLE        22
-#define MENU_FONT_NAME_INFO         "/usr/games/menu_resources/unifont-15.0.01.ttf"
+#define MENU_FONT_NAME_INFO         "/usr/games/layouts/Bigcup/unifont-15.0.01.ttf"
 #define MENU_FONT_SIZE_INFO         16
-#define MENU_FONT_NAME_SMALL_INFO   "/usr/games/menu_resources/unifont-15.0.01.ttf"
+#define MENU_FONT_NAME_SMALL_INFO   "/usr/games/layouts/Bigcup/unifont-15.0.01.ttf"
 #define MENU_FONT_SIZE_SMALL_INFO   13
 #define MENU_PNG_BG_PATH            "/usr/games/menu_resources/zone_bg.png"
 #define MENU_PNG_ARROW_TOP_PATH     "/usr/games/menu_resources/arrow_top.png"
@@ -790,8 +790,8 @@ static void menu_screen_refresh(SDL_Surface *screen, int menuItem, int prevItem,
 			}
 			else{
 				if(menu_confirmation){
-					sprintf(text_tmp, "Are you sure?");
-					text_surface = TTF_RenderText_Blended(menu_info_font, text_tmp, text_color);
+					sprintf(text_tmp, "是否確定?");
+					text_surface = TTF_RenderUTF8_Blended(menu_info_font, text_tmp, text_color);
 					text_pos.x = (screen->w - MENU_ZONE_WIDTH)/2 + (MENU_ZONE_WIDTH - text_surface->w)/2;
 					text_pos.y = screen->h - MENU_ZONE_HEIGHT/2 - text_surface->h/2 + 2*padding_y_from_center_menu_zone;
 					SDL_BlitSurface(text_surface, NULL, screen, &text_pos);
@@ -802,7 +802,7 @@ static void menu_screen_refresh(SDL_Surface *screen, int menuItem, int prevItem,
 #ifdef HAS_MENU_RO_RW
 		case MENU_TYPE_RO_RW:
 			sprintf(text_tmp, "%s", read_write?"唯讀":"可讀寫");
-			text_surface = TTF_RenderUTF8_Blended(menu_title_font, text_tmp, text_color);
+			text_surface = TTF_RenderText_Blended(menu_title_font, text_tmp, text_color);
 			text_pos.x = (screen->w - MENU_ZONE_WIDTH)/2 + (MENU_ZONE_WIDTH - text_surface->w)/2;
 			text_pos.y = screen->h - MENU_ZONE_HEIGHT/2 - text_surface->h/2;
 			SDL_BlitSurface(text_surface, NULL, screen, &text_pos);
